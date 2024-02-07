2f1efb935188d952924977844514f0ec7cf8d256
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 13:25:57 +0500

    adapted ui to fetch questions from quizbrain and show up in canvas

--- ui.py
+++ ui.py
@@ -1,16 +1,19 @@
 THEME_COLOR = "#375362"
 from tkinter import *
+from quiz_brain import QuizBrain
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.configure(padx=20, pady=20, bg=THEME_COLOR)
-        self.score_label = Label(text='Score: 0', font=('SF Display', 15, 'normal'), bg=THEME_COLOR, fg='white')
+        self.score_label = Label(text='Score: 0', font=('SF Pro Display', 15, 'normal'), bg=THEME_COLOR, fg='white')
         self.score_label.grid(row=0, column=1, padx=20, pady=20)
         self.question_canvas = Canvas(bg='white', height=250, width=300)
         self.question_canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
-        self.question_canvas.create_text(150, 125, text='Amazon acquired Twitch in August 2014 for $970 million dollars.', font=('SF Pro Display', 20, 'italic'), width=270)
+        self.question_text = self.question_canvas.create_text(150, 125, text='Amazon acquired Twitch in August 2014 for $970 million dollars.', font=('SF Pro Display', 20, 'italic'), width=270)
+        self.show_next_question()
         correct_img = PhotoImage(file='images/true.png')
         false_img = PhotoImage(file='images/false.png')
         self.correct_button = Button(image=correct_img)
@@ -20,3 +23,6 @@ class QuizInterface:
 
 
         self.window.mainloop()
+    def show_next_question(self):
+        q_text = self.quiz.next_question()
+        self.question_canvas.itemconfig(self.question_text, text=q_text)
