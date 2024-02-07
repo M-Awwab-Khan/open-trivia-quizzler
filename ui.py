6c3666337eb6b73f329a285a4546870ffeda7af0
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 13:01:40 +0500

    basic tkinter ui setup using OOP

--- ui.py
+++ ui.py
@@ -1,2 +1,11 @@
 THEME_COLOR = "#375362"
+from tkinter import *
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Quizzler')
+
+
+
+        self.window.mainloop()
