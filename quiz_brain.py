7326ca08675d3bc02ec04039570475b640e96699
Awwab <mawwabkhank2006@gmail.com>
2024-02-07 13:26:42 +0500

    modified next_question to return question

--- quiz_brain.py
+++ quiz_brain.py
@@ -13,9 +13,9 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
-        self.check_answer(user_answer)
-
+        # user_answer = input(f"Q.{self.question_number}: {html.unescape(self.current_question.text)} (True/False): ")
+        # self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {html.unescape(self.current_question.text)}"
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
